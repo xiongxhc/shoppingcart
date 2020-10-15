08c53711c02a5efcda0041d20cbf38c6ab41e476
Chris Xiong <xionghc713@gmail.com>
2020-10-16 01:57:19 +0800

    create Item component

---
+++ src/components/Grid/Item.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Item = ({item}) => {
+    return (
+        <div key={item.id}>
+            <div className="item">
+                <a href={'#' + item.id}>
+                    <img src={item.photo} alt={item.name} />
+                    <p>
+                        {item.name}
+                    </p>
+                </a>
+                <div>
+                    <div className="pricetag">
+                        <div>${item.price}</div>
+                    </div>
+                    <button className="addtocart">
+                        <div>Add To Cart</div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item;
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Item from './Item'
 import './index.scss';
 
 class Grid extends Component {
@@ -6,25 +7,8 @@ class Grid extends Component {
 		return (
 			<div className="grid">
 				<div className="products">
-					{this.props.products.map( item => (
-						<div key={item.id}>
-							<div className="item">
-								<a href={'#'+item.id}>
-									<img src={item.photo} alt={item.name} />
-									<p>
-										{item.name}
-									</p>
-								</a>
-								<div>
-									<div className="pricetag">
-										<div>${item.price}</div>
-									</div>
-									<button className="addtocart">
-										<div>Add To Cart</div>
-									</button>
-								</div>
-							</div>
-						</div>
+					{this.props.products.map( each => (
+						<Item item={each} />
 					))}
 				</div>
 			</div>
