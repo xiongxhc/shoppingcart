3d4abd2c5eb5173d6fa2056ca6bba1460dd6fa35
Chris Xiong <xionghc713@gmail.com>
2020-10-16 07:26:20 +0800

    added Cart icon ui and Logout icon ui

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './index.scss';
 import Title from './Title.js'
+import { CartIcon, Logout } from '../../icons'
 
 const Header = () => {
 	return (
 		<header className="header">
-			<Title text={"EStore"} />
+			<div className="title">
+				<Title text={"EStore"} />
+			</div>
+			<div className="righticons">
+				<a href='/' className="cart">
+					<CartIcon width={25} color={"#FFF"}/>
+					<p>(0)</p>
+				</a>
+				<a href='/' className="logout">
+					<Logout width={25} color={"#FFF"}/>
+					<p>Logout</p>
+				</a>
+			</div>
 		</header>
 	);
 }
