da6367159df6ca22f11c6249e34225a19caee2cf
Chris Xiong <xionghc713@gmail.com>
2020-10-18 22:42:54 +0800

    added login to view cart

--- src/components/CartList/index.js
+++ src/components/CartList/index.js
@@ -6,39 +6,47 @@ import CartItem from './CartItem.js'
 
 class CartList extends Component {
 	render() {
-		const { cartItems } = this.props
+		const { login, cartItems } = this.props
 		var sum = 0
 		cartItems.forEach(each => {
 			sum += Number(each.price) * Number(each.count)
 		});
 		return (
 			<div>
-				<div className="cartlist">
-					<div className="cartproducts">
-						{cartItems.length !== 0 ? cartItems.map(each => (
-							<CartItem key={each.id} item={each} />
-						)) : 
-							<div className="emptycart">
-								<p>Your cart is empty</p>
+				{ login ?
+					<React.Fragment>
+						<div className="cartlist">
+							<div className="cartproducts">
+								{cartItems.length !== 0 ? cartItems.map(each => (
+									<CartItem key={each.id} item={each} />
+								)) : 
+									<div className="emptycart">
+										<p>Your cart is empty</p>
+									</div>
+								}
 							</div>
-						}
-					</div>
-				</div>
-				<div className="checkout">
-					<div className="values">
-						<div>
-							<p>Total Item</p>
-							<p>{cartItems.length}</p>
 						</div>
-						<div>
-							<p>Total Payment</p>
-							<p>${sum.toFixed(2)}</p>
-						</div>
+						<div className="checkout">
+							<div className="values">
+								<div>
+									<p>Total Item</p>
+									<p>{cartItems.length}</p>
+								</div>
+								<div>
+									<p>Total Payment</p>
+									<p>${sum.toFixed(2)}</p>
+								</div>
+							</div>
+							<div>
+								<button>Checkout</button>
+							</div>
+						</div> 
+					</React.Fragment>
+					:
+					<div className="emptycart">
+						<p>Please login to view cart</p>
 					</div>
-					<div>
-						<button>Checkout</button>
-					</div>
-				</div>
+				}
 			</div>
 		);
 	}
@@ -46,6 +54,7 @@ class CartList extends Component {
 
 const mapStateToProps = (state) => {
 	return {
+		login: state.user.login,
 		cartItems: state.cart.cartItems,
 	};
 }
